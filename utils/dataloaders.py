053ab7eef5740bdd568c90154a667036592f079b
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-03-23 08:48:22 +0200

    Fix letterbox mask bug

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -674,7 +674,8 @@ class LoadImagesAndLabels(Dataset):
             shape = self.batch_shapes[self.batch[index]] if self.rect else self.img_size  # final letterboxed shape
             img, ratio, pad = letterbox(img, shape, auto=False, scaleup=self.augment)
             mask_shape = [shape[0] // 2, shape[1] // 2] if self.rect else self.img_size // 2
-            seg, _, _ = letterbox(img, mask_shape, auto=False, scaleup=self.augment, color=(0, 0, 0))
+            print('\n---Seg Mask before letterbox', np.all((seg > 0)), '-----\n')
+            seg, _, _ = letterbox(seg, mask_shape, auto=False, scaleup=self.augment, color=(0, 0, 0))
             print('\n---Seg Mask after letterbox', np.all((seg > 0)), '-----\n')
             shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP rescaling
 
