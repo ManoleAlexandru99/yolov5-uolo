72d83c4b220db85ea2d330b3ade5632c830ea48d
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-03-24 08:21:38 +0200

    Print code version

--- train.py
+++ train.py
@@ -511,6 +511,7 @@ def parse_opt(known=False):
 
 
 def main(opt, callbacks=Callbacks()):
+    print('\n---------- VERSION:', '#0005', '----------\n')
     # Checks
     if RANK in {-1, 0}:
         print_args(vars(opt))
