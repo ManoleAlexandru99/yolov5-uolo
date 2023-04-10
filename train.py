29750868f5cbb209b8d62025f192ef040d261e44
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-04-10 16:00:10 +0300

    Anomaly Detection Reenabled

--- train.py
+++ train.py
@@ -269,6 +269,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 f"Logging results to {colorstr('bold', save_dir)}\n"
                 f'Starting training for {epochs} epochs...')
 
+    torch.autograd.set_detect_anomaly(True)
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         callbacks.run('on_train_epoch_start')
         model.train()
