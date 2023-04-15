6fb35250d35465741acf58891cf66e68c1167641
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-04-15 17:16:21 +0300

    extra image log

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -695,9 +695,12 @@ class LoadImagesAndLabels(Dataset):
                 seg, _, _ = random_perspective(seg, labels, degrees=hyp['degrees'], translate=hyp['translate'], scale=hyp['scale'],
                                                shear=hyp['shear'], perspective=hyp['perspective'], random_parameters=random_parameters,
                                                border_value=(0, 0, 0))
-
-        cv2.imwrite('runs/image' + str(index) + '.jpg', img)
-        cv2.imwrite('runs/mask' + str(index) + '.png', seg)
+        if index < 50:
+            cv2.imwrite('runs/image' + str(index) + '.jpg', img)
+            cv2.imwrite('runs/mask' + str(index) + '.png', seg)
+            h, w = seg.shape[0], seg.shape[1]
+            resized = cv2.resize(seg, (h, w), interpolation=cv2.INTER_AREA)
+            cv2.imwrite('runs/maskr' + str(index) + '.png', seg)
         nl = len(labels)  # number of labels
         if nl:
             labels[:, 1:5] = xyxy2xywhn(labels[:, 1:5], w=img.shape[1], h=img.shape[0], clip=True, eps=1E-3)
