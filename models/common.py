7bdb86638d0482a3ef43cf7561fd7f29ce29419f
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-04-17 08:51:53 +0300

    Reduced no. of channels in decoding path

--- models/common.py
+++ models/common.py
@@ -852,9 +852,9 @@ class Seg(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
         print('SEG in channels: ', in_channels)
-        self.cv1 = Conv(in_channels, 128, k=3)
+        self.cv1 = Conv(in_channels, 64, k=3)
         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-        self.cv2 = Conv(128, 64, k=3)
+        self.cv2 = Conv(64, 64, k=3)
         self.cv3 = Conv(64, 32, k=3)
         self.cv4 = Conv(32, 1, act=False)
         self.relu = nn.ReLU()
