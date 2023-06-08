829fb7eadb6e6ec0295ecc72462ab11f71f854a1
ManoleAlexandru99 <manole.alexandru99@yahoo.ro>
2023-06-08 12:22:02 +0300

    Last Layer 16 Channels

--- models/common.py
+++ models/common.py
@@ -859,8 +859,8 @@ class Seg(nn.Module):
 
         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
         self.cv2 = Conv(192, 48, k=3)
-        self.cv3 = Conv(96, 8, k=3)
-        self.cv4 = Conv(8, 1, act=False)
+        self.cv3 = Conv(96, 16, k=3)
+        self.cv4 = Conv(16, 1, act=False)
         self.relu = nn.ReLU()
         self.dropout_normal = nn.Dropout(0.5)
 
